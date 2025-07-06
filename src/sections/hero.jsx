6f9d7c904762e5b40efc789b419bff59132d6a4d
JinghIzKing <jsinghcs22@gmail.com>
2025-07-06 11:20:37 -0400

    Added hero containers

---
+++ src/sections/hero.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section id="hero">
+      {/* CORNER PATTERN IMAGE */}
+      <div>
+        <img className="hue-rotate-300 contrast-300 saturate-100" src="/images/bg.png" alt="background" />
+      </div>
+      {/* HERO */}
+      <div>
+        <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">WORK IN PROGRESS</header>
+        {/* HERO CONTENT */}
+        <div> 
+
+        </div>
+        <div>
+          
+        </div>
+        {/* 3D MODEL */}
+      </div>
+    </section>
+
+
+  )
+}
+
+export default Hero
