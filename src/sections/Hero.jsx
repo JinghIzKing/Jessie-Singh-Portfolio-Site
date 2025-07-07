e9b758476b1e89c378203b215a8c8e84ab35dddb
JinghIzKing <jsinghcs22@gmail.com>
2025-07-07 17:16:16 -0400

    Fixed navbar layout

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,26 +2,9 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <section id="hero" className="relative overflow-hidden">
-      {/* CORNER PATTERN IMAGE */}
-      <div className="absolute top-0 left-0 z-10">
-        <img className="hue-rotate-300 contrast-300 saturate-100" src="/images/bg.png" alt="background" />
-      </div>
-      {/* HERO */}
-      <div>
-        <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">WORK IN PROGRESS</header>
-        {/* HERO CONTENT */}
-        <div> 
+    <section id="hero" className="relative">
 
-        </div>
-        <div>
-          
-        </div>
-        {/* 3D MODEL */}
-      </div>
     </section>
-
-
   )
 }
 
--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -2,21 +2,22 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className="flex justify-center gap-20">
-        <img className="w-20 h-20" src="public\images\fav.png" alternate="logo"></img>
-        <link rel="icon" type="image/svg+xml" href="public\images\fav.png" />
-        <h1>Jessie Singh</h1>
-        <div>
-            Projects
+    <div className="sticky flex justify-evenly items-center border border-gray-300">
+        <div className="flex items-center gap-2">
+            <img className="w-20 h-20 p-3 rounded-3xl" src="public\images\fav.png" alternate="logo"></img>
+            <h1 className="text-2xl">Jessie Singh</h1>
         </div>
-        <div>
-            Experience
+        <div className="flex items-center">
+            <h1>Projects</h1>
         </div>
-        <div>
-            About Me
+        <div className="flex items-center">
+            <h1>Experience</h1>
+        </div>
+        <div className="flex items-center">
+            <h1>About Me</h1>
         </div>
         <button>
-            Contact Me!
+            <h1>Contact Me!</h1>
         </button>
     </div>
   )
